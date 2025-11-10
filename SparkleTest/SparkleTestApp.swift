ee326eb58b950221beea112a5224df9e06caebe5
Stefan Horner <stefan.horner@umotif.com>
2025-11-10 19:47:19 +0000

    Initial Commit

---
+++ SparkleTest/SparkleTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparkleTestApp.swift
+//  SparkleTest
+//
+//  Created by Stefan Horner on 10/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct SparkleTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
